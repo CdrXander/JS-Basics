51430fd32986502efbf35e553bfd72b1d2a71a09
CdrXander <xackhackman@gmail.com>
2016-11-04 09:31:33 -0600

    Completed project 1

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -4,13 +4,22 @@ var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
-  //Code Here
+
+function isTyler(strIn) {
+	if(strIn === "Tyler") {
+		return true;
+	}
+	return false;
+}
 
 //////////////////PROBLEM 2////////////////////
 
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
+function getName() {
+	var name = prompt("Who dare disturb my slumber?");
+	return name;
+}
 
   //Code Here
 
@@ -22,12 +31,15 @@ var name = 'Tyler';
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
+function welcome() {
+	var name = getName();
+	alert("Welcome, " + name);
+}
+
   //Code Here
 
 
 //////////////////PROBLEM 4////////////////////
-
-
 
 
 //What is the difference between arguments and parameters?
@@ -48,21 +60,23 @@ var name = 'Tyler';
 
 //////////////////PROBLEM 6////////////////////
 
-
-
 //Create a function called myName that returns your name
 
   //Code Here
-  
+function myName() {
+	return "Xander Hacking";
+}  
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+
+var newMyName = function(){return "Xander Hacking";}
+
 
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName());
 
 //////////////////PROBLEM 7////////////////////
 
@@ -72,8 +86,17 @@ var name = 'Tyler';
 
   //Code Here
 
+  function outerFn() {
+  	return function(){return "Xander Hacking"};
+  }
+
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
 
+var innerFn = outerFn();
+
+
 //Now invoke innerFn.
+
+innerFn();
